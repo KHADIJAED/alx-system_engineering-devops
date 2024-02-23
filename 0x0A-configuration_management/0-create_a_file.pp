828e0e67341c67fe5345bde50f634488f94699e8
KHADIJAED <khadija.eddraiji@gmail.com>
2024-02-23 14:52:28 -0800

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creates a file in /tmp
 
-file { ' /tmp/school':
+file { '/tmp/school':
   content => 'I love puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
 }
+
