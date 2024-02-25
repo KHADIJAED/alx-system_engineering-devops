a20d6dd7bf607ae15681e987df6e2f40defd3b56
KHADIJAED <khadija.eddraiji@gmail.com>
2024-02-25 13:37:25 -0800

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
+
+file { 'ect/ssh/ssh_cofig':
+        ensure => present,
+
+content =>"
+          
+        #SSH client configuration
+        host*
+        IdentityFile ~.ssh/school
+        PasswordAuthentication no
+
+}     
+                   
